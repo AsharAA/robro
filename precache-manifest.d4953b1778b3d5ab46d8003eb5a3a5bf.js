418722a27987f2af5fe5a5d9a973da7118b6b4a7
AD ASH <fashionest8@gmail.com>
2022-11-09 03:36:03 -0800

    Updates

---
+++ precache-manifest.d4953b1778b3d5ab46d8003eb5a3a5bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f929db624404106db0c",
+    "url": "/robro/static/css/main.c6cef791.chunk.css"
+  },
+  {
+    "revision": "7f929db624404106db0c",
+    "url": "/robro/static/js/main.1f620852.chunk.js"
+  },
+  {
+    "revision": "d2120a450ef1737ecc5c",
+    "url": "/robro/static/js/runtime~main.8e4fbe31.js"
+  },
+  {
+    "revision": "988f1528e8339f322625",
+    "url": "/robro/static/css/2.b65b5852.chunk.css"
+  },
+  {
+    "revision": "988f1528e8339f322625",
+    "url": "/robro/static/js/2.ef93a9b1.chunk.js"
+  },
+  {
+    "revision": "8fa34ba00032514f3d70",
+    "url": "/robro/static/js/3.490731cf.chunk.js"
+  },
+  {
+    "revision": "88bdd9f551a3b01cb0b39033012b717a",
+    "url": "/robro/static/media/Spy.88bdd9f5.woff"
+  },
+  {
+    "revision": "be2fd32a054e39c9d641ba06167e3f05",
+    "url": "/robro/index.html"
+  }
+];
